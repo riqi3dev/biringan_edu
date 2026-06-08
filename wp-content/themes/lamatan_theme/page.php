39c94dcb10531094492cbc99aaa0ec3d01f26b27
Enrique Pacudan <riki.iiidev@gmail.com>
2026-06-09 06:13:26 +0800

    handling of permalinks and side menu navigation

--- wp-content/themes/lamatan_theme/page.php
+++ wp-content/themes/lamatan_theme/page.php
@@ -16,19 +16,45 @@ while (have_posts()) {
     </div>
 
     <div class="container container--narrow page-section">
-        <div class="metabox metabox--position-up metabox--with-home-link">
-            <p>
-                <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
-            </p>
+        <?php
+        
+        $parentId = wp_get_post_parent_id(get_the_ID());
+
+        if ($parentId) { ?>
+            <div class="metabox metabox--position-up metabox--with-home-link">
+                <p>
+                    <a class="metabox__blog-home-link" href="<?php echo get_permalink($parentId); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($parentId); ?></a> <span class="metabox__main"><?php the_title(); ?></span>
+                </p>
+            </div>
+        <?php } ?>
+
+        <?php 
+        // handles pages that is not a parent nor child page
+        $permaLink = get_pages(array('child_of' => get_the_ID()));
+        if ($parentId || $permaLink) { ?>
+
+        <!-- if ($parentId) { ?> -->
+
+        <div class="page-links">
+            <h2 class="page-links__title"><a href="<?php echo get_permalink($parentId); ?>"><?php echo get_the_title($parentId); ?></a></h2>
+            <ul class="min-list">
+                <?php
+                
+                if ($parentId) {
+                    $childId = $parentId;
+                } else {
+                    $childId = get_the_ID();
+                }
+
+                wp_list_pages(array(
+                    'title_li' => null,
+                    'child_of' => $childId,
+                    'sort_column' => 'menu_order'
+                )); ?>
+            </ul>
         </div>
 
-        <!-- <div class="page-links">
-            <h2 class="page-links__title"><a href="#">About Us</a></h2>
-            <ul class="min-list">
-                <li class="current_page_item"><a href="#">Our History</a></li>
-                <li><a href="#">Our Goals</a></li>
-            </ul>
-        </div> -->
+        <?php } ?>
 
         <div class="generic-content">
             <?php the_content(); ?>
